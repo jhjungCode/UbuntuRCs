2b08dfb16d865dbce62b5f036ed989ef67d97162
JongHwa Jung <jhjung0203@gmail.com>
2019-12-17 15:58:22 +0900

    add mkdir command

--- makelnk.sh
+++ makelnk.sh
@@ -4,5 +4,6 @@ ln -sf $(pwd)/inputrc  ~/.inputrc
 ln -sf $(pwd)/bashrc   ~/.bashrc
 ln -sf $(pwd)/screenrc   ~/.screenrc
 
-mkdir  ~/.config/nvim
+mkdir ~/.config
+mkdir ~/.config/nvim
 ln -sf $(pwd)/vimrc ~/.config/nvim/init.vim
